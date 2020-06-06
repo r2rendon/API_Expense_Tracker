f920d17e0c9333cb71f08025238db5b7f976e458
Arturo Rendon <41806761+r2rendon@users.noreply.github.com>
2020-06-05 18:10:23 -0600

    MongoDB Connections

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const colors = require("colors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+app.use(express.json());
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("DB Connected");
+};
 
 app.listen(5000, () => {
   console.log("\tListening on port 5000!!".green);
+  connectDB();
 });
